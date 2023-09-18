f0860994f4d50d362df1338b39239ef82bd28d67
lovehifi <81028393+lovehifi@users.noreply.github.com>
2023-09-18 14:09:15 +0700

    Update install-archlinux.sh

--- install-archlinux.sh
+++ install-archlinux.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 cd ~
+echo "Install Java"
 sudo pacman -S --noconfirm jre-openjdk
+echo "Unzip"
 sudo pacman -S --noconfirm unzip
-
+echo "Download BubbleUPnPTetherClient"
 wget https://bubblesoftapps.com/bubbleupnptetherclient/BubbleUPnPTetherClient-0.9.2.zip
 sudo unzip BubbleUPnPTetherClient-0.9.2.zip -d /opt/upnp
 
@@ -32,5 +34,4 @@ sudo systemctl enable bupnp.service
 sudo systemctl restart bupnp.service
 #sudo systemctl status bupnp.service
 
-# wget -N -O install-archlinux.sh https://raw.githubusercontent.com/lovehifi/lmsbub/main/install-archlinux.sh | sh
 # wget -O - https://raw.githubusercontent.com/lovehifi/lmsbub/main/install-archlinux.sh | sh
